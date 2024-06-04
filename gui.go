150f8c5bf52341cb9965e5f205dc74e28b4c903a
Ryan Gauthier <ryan.gauthier@openenergysolutions.com>
2024-06-04 12:10:41 -0700

    end of part 2

--- gui.go
+++ gui.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"image/color"
+	// "log"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
+
+type gui struct {
+	win fyne.Window
+	//Project Label
+	title binding.String
+	// directory *widget.Label
+}
+	
 
 func makeBanner() fyne.CanvasObject {
 	toolbar := widget.NewToolbar(
@@ -19,17 +30,51 @@ func makeBanner() fyne.CanvasObject {
 	return container.NewStack(toolbar, container.NewPadded(logo))
 }
 
-func makeGUI() fyne.CanvasObject {
+func (g* gui) makeGUI() fyne.CanvasObject {
 	top := makeBanner()
 	left := widget.NewLabel("Left")
 	right := widget.NewLabel("Right")
 
 	// content := widget.NewLabel("CONTENT")
-	content := canvas.NewRectangle(color.White)
+	directory := widget.NewLabelWithData(g.title)
+	// directory := canvas.NewLabel("Directory")
+	content := container.NewStack(canvas.NewRectangle(color.White), directory)
 	
 	dividers := [3]fyne.CanvasObject{
 		widget.NewSeparator(), widget.NewSeparator(), widget.NewSeparator(),
 	}
 	objs := []fyne.CanvasObject{top, left, right, content, dividers[0], dividers[1], dividers[2]}
 	return container.New(newFysionLayout(top, left, right, content, dividers), objs...)
+}
+
+func (g* gui) openProjectDialog() {
+	// Open a file dialog to select a project
+	dialog.ShowFolderOpen(func(dir fyne.ListableURI, err error) {
+		// log.Println("Selected: ", dir)
+		if err != nil {
+			dialog.ShowError(err, g.win)
+			return
+		}
+		if dir == nil {
+			g.openProjectDialog()
+			return
+		}
+
+		g.openProject(dir)
+
+	}, g.win)
+}
+
+func (g *gui) makeMenu() *fyne.MainMenu {
+	file := fyne.NewMenu("File",
+		fyne.NewMenuItem("Open Project", g.openProjectDialog),
+	)
+	return fyne.NewMainMenu(file)
+}
+
+func (g *gui) openProject(dir fyne.ListableURI) {
+	name := dir.Path()
+
+	// Load the project
+	g.title.Set(name)
 }
--- main.go
+++ main.go
@@ -1,15 +1,52 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"path/filepath"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/storage"
 )
 
 func main() {
 	a := app.New()
 	a.Settings().SetTheme(newFysionTheme())
 	w := a.NewWindow("Fysion App")
-	w.Resize(fyne.NewSize(800, 600))
-	w.SetContent(makeGUI())
+	w.Resize(fyne.NewSize(1024, 768))
+	ui := &gui{win: w, title: binding.NewString()}
+	w.SetContent(ui.makeGUI())
+	w.SetMainMenu(ui.makeMenu())
+	// ui.openProject()
+	ui.title.AddListener(binding.NewDataListener(func() {
+		name, _ := ui.title.Get()
+		w.SetTitle("Fysion App - " + name)
+	}))
+	flag.Usage = func() {
+		fmt.Println("Usage: fysion [project directory]")
+	}
+	flag.Parse()
+	if flag.NArg() > 0 {
+		flagPath := flag.Arg(0)
+		flagPath, err := filepath.Abs(flagPath)
+		if err != nil {
+			fmt.Println("Error resolving project path", err)
+			return
+		}
+
+		dirURI := storage.NewFileURI(flagPath)
+		dirPath, err := storage.ListerForURI(dirURI)
+		if err != nil {
+			fmt.Println("Error opening project", err)
+			return
+		}
+
+		ui.openProject(dirPath)
+	} else {
+		ui.openProjectDialog()
+	}
+
 	w.ShowAndRun()	
 }
